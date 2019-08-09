6ca8e58d7c63a748061a51d0261ea19171e40a26
DavidHanover <DavidHanover89@gmail.com>
2019-08-09 00:34:00 -0700

    created client production build folder

---
+++ public/precache-manifest.fbd9263337e4611fbe01fa6ec6c73a76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1eb279a91601d00c1417e35705d7179",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb57e0d03346bda7a0ae",
+    "url": "/static/css/main.3c33e02e.chunk.css"
+  },
+  {
+    "revision": "ef73ee6c356f4f40d371",
+    "url": "/static/js/2.997a0251.chunk.js"
+  },
+  {
+    "revision": "fb57e0d03346bda7a0ae",
+    "url": "/static/js/main.3d2ffa74.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
